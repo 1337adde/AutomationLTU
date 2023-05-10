74116f4c557f924e26077bd890228da1feca7814
Adam <hanada-2@student.ltu.se>
2023-05-10 10:17:17 +0200

    Added test for ExamInfo

--- src/test/java/org/example/ExaminationInformationTest.java
+++ src/test/java/org/example/ExaminationInformationTest.java
@@ -1,8 +1,21 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ExaminationInformationTest {
-
+    @Test
+    void pathExists() {
+        ExaminationInformation.find();
+        Path examinationPath = Paths.get("target/downloads/final_examination.jpeg");
+        assertTrue(Files.exists(examinationPath));
+    }
 
 }
